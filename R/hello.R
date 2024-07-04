f25a6242ee0967ff550367ce53505351bc71ac09
mauricio04780 <fumex2004@gmail.com>
2024-07-04 16:43:53 -0500

    primer commit

--- R/hello.R
+++ R/hello.R
@@ -12,3 +12,20 @@ digitos <- function(Numero_3_digitos){
     print("No es un numero de 3 digitos")
   }
 }
+may <- function(a, b, c) {
+  if (a >= b & a >= c) {
+    return(a)
+  } else if (b >= a & b >= c) {
+    return(b)
+  } else {
+    return(c)
+  }
+}
+
+# Ejemplo de uso
+num1 <- 10
+num2 <- 5
+num3 <- 8
+
+mayor <- may(num1, num2, num3)
+cat(sprintf("El mayor número es: %d\n", mayor))
