562a76a1e436fa317f1ab23c0d135895351c7b5b
mauricio04780 <fumex2004@gmail.com>
2024-07-04 16:10:10 -0500

    primer commit

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+digitos <- function(Numero_3_digitos){
+  N <- Numero_3_digitos
+  if(N >= 100 & n < 1000){
+    dig1 <- trunc(N/100)
+    X1 <- N %% 100
+    dig2 <- trunc(x1/10)
+    dig3 <- x1 %% 10
+    s <- dig1+dig2+dig3
+    print("La suma de los digitos es: ")
+    print(s)
+  }else{
+    print("No es un numero de 3 digitos")
+  }
+}
